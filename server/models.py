d24fae6c3908bee761e52766192e701be2645bd9
shamim23m <shamimmohamud13@gmail.com>
2025-01-31 21:16:47 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -1,3 +1,4 @@
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
@@ -21,8 +22,10 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    pizzas = db.relationship("RestaurantPizza", back_populates="restaurant")
 
     # add serialization rules
+    serialize_rules = ('-pizzas',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +39,10 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    restaurants = db.relationship("RestaurantPizza", back_populates="pizza")
 
     # add serialization rules
+    serialize_rules = ('-restaurants',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -49,11 +54,22 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+
     # add relationships
+    restaurant = db.relationship("Restaurant", back_populates="pizzas", cascade="all, delete-orphan", single_parent=True)
+    pizza = db.relationship("Pizza", back_populates="restaurants", cascade="all, delete-orphan", single_parent=True)
 
     # add serialization rules
+    serialize_rules = ('-restaurant', '-pizza')
 
     # add validation
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError('Price must be between 1 and 30')
+        return value 
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
